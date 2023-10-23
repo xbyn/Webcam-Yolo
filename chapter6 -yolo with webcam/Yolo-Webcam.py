ff40b0d4a5262e44b3d9f622db32d71f16540924
xbyn <albogamiibayan@gmail.com>
2023-10-23 20:41:53 +0300

    Initial commit

---
+++ chapter6 -yolo with webcam/Yolo-Webcam.py
@@ -0,0 +1,51 @@
+from ultralytics import YOLO
+import cv2
+import cvzone  # etaction
+import math
+
+
+# cap = cv2.VideoCapture(0) # for webcam
+# cap.set(3, 1280)  # width for webcam
+# cap.set(4, 720)   # hight for webcam
+
+cap = cv2.VideoCapture("../Videos/people.mp4")  # for videos
+
+model = YOLO("../Yolo-weights/Yolov8n.pt")
+
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"]
+
+while True:
+    success, img = cap.read()
+    results = model(img, stream=True)  # recomended to use stream=true
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            w, h = x2-x1, y2-y1
+            cvzone.cornerRect(img, (x1, y1, w, h), t=3, colorR=(255, 0, 255), colorC=(255, 0, 255))
+
+            # confident
+            conf = math.ceil((box.conf[0]*100))/100
+
+            # Class name
+
+            cls = int(box.cls[0])
+            cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=1, thickness=1)
+
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
+
+
